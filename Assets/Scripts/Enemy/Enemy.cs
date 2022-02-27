7b2f87f4b3ac0cec0ab71e0b7372dc27025602d6
TheLukCraft <luk1997r@o2.pl>
2022-02-27 11:19:01 +0100

    Enemy move and attack

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private string currentState = "IdleState";
+    private Transform target;
+    public float chaseRange = 5;
+    public Animator animator;
+    public float speed = 3;
+    public float attackRange = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, target.position);
+
+        if(currentState == "IdleState")
+        {
+            if (distance < chaseRange)
+                currentState = "ChaseState";
+        }else if(currentState == "ChaseState")
+        {
+            animator.SetTrigger("chase");
+            animator.SetBool("isAttacking", false);
+
+            if(distance < attackRange)
+            {
+                currentState = "AttackState";
+            }
+
+            if(target.position.x > transform.position.x)
+            {
+                transform.Translate(transform.right * speed * Time.deltaTime);
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+            }else
+            {
+                transform.Translate(-transform.right * speed * Time.deltaTime);
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
+        }else if (currentState == "AttackState")
+        {
+            animator.SetBool("isAttacking", true);
+            if (distance > attackRange)
+            {
+                currentState = "ChaseState";
+            }
+        }
+    }
+}
